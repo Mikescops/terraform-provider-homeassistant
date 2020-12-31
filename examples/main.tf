0720295791565040db48099a8deb9e0a61f65462
Eleonore Carpentier <eleonore.carpentier@solarisbank.de>
2020-12-31 17:44:59 +0100

    Holiday themes tf code

--- examples/main.tf
+++ examples/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "ha" {
   host_url = "https://home.pixelswap.fr:8888/api"
-  # bearer_token = "xxxx"
+//  bearer_token = "xx.xxx.xx"
 }
 
 module "magic_ha_lights" {
@@ -31,3 +31,12 @@ output "magic_ha_mediaplayer_initial" {
 // output "magic_ha_lights_next" {
 //   value = module.magic_ha_lights.tv_light_info
 // }
+
+
+module "holiday_mood" {
+  source = "./holiday_mood"
+  enable_lamps = false
+  enable_google_home = true
+  enable_tv = true
+  desired_holiday_mood = "christmas"
+}
